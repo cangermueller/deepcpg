8bc3883e01b895fad44eac085dc66dc8bf0bdb27
Christof Angermueller <cangermueller@gmail.com>
2016-09-19 18:27:41 +0100

    Purge legacy files

--- deepcpg/models/dnv/R/utils.R
+++
@@ -1,9 +0,0 @@
-format_target <- function(d) {
-    d <- d %>%
-      separate(target, c('cell_type', 'wlen', 'type'), by='_', remove=F) %>%
-      mutate(
-        cell_type=factor(cell_type, levels=c('2i', 'ser'), labels=c('2i', 'serum')),
-        wlen=as.integer(sub('w', '', wlen))
-      )
-    return (d)
-}
