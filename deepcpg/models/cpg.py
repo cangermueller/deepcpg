3a3ba797a139724ec9f3516bf85503f2cd80277b
Christof Angermueller <cangermueller@gmail.com>
2016-11-12 15:10:24 +0000

    Remove cpg models

--- deepcpg/models/cpg.py
+++ deepcpg/models/cpg.py
@@ -99,49 +99,5 @@ class Cpg04(CpgModel):
         return km.Model(input=inputs, output=x, name=self.name)
 
 
-class Cpg05(CpgModel):
-    """LSTM with two layers (2x128, 2x256).
-    1285633 parameters"""
-
-    def __call__(self, inputs):
-        x = self._merge_inputs(inputs)
-
-        w_reg = kr.WeightRegularizer(l1=self.l1_decay, l2=self.l2_decay)
-        x = kl.Bidirectional(kl.LSTM(128, W_regularizer=w_reg,
-                                     return_sequences=True),
-                             merge_mode='concat')(x)
-
-        w_reg = kr.WeightRegularizer(l1=self.l1_decay, l2=self.l2_decay)
-        x = kl.Bidirectional(kl.LSTM(256, W_regularizer=w_reg))(x)
-        x = kl.Dropout(self.dropout)(x)
-
-        return km.Model(input=inputs, output=x, name=self.name)
-
-
-class Cpg06(CpgModel):
-    """GRU with three layers (2x128, 2x128, 2x256)
-    1260033 parameters"""
-
-    def __call__(self, inputs):
-        x = self._merge_inputs(inputs)
-
-        w_reg = kr.WeightRegularizer(l1=self.l1_decay, l2=self.l2_decay)
-        x = kl.Bidirectional(kl.GRU(128, W_regularizer=w_reg,
-                                    return_sequences=True),
-                             merge_mode='concat')(x)
-
-        w_reg = kr.WeightRegularizer(l1=self.l1_decay, l2=self.l2_decay)
-        x = kl.Bidirectional(kl.GRU(128, W_regularizer=w_reg,
-                                    return_sequences=True),
-                             merge_mode='concat')(x)
-
-        w_reg = kr.WeightRegularizer(l1=self.l1_decay, l2=self.l2_decay)
-        x = kl.Bidirectional(kl.GRU(256, W_regularizer=w_reg))(x)
-
-        x = kl.Dropout(self.dropout)(x)
-
-        return km.Model(input=inputs, output=x, name=self.name)
-
-
 def get(name):
     return get_from_module(name, globals())
