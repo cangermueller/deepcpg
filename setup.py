900b4fdbeb7b6c0a2e2ad615e1824217459ccb50
Christof Angermueller <cangermueller@gmail.com>
2017-05-01 09:51:59 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='deepcpg',
-      version='1.0.3',
+      version='1.0.4',
       description='Deep learning for predicting CpG methylation',
       long_description=read('README.rst'),
       author='Christof Angermueller',
